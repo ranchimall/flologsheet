b71d7609a32a26b59b5aa3db3016780cc12741b8
sairajzero <sairaj0630@gmail.com>
2023-05-02 20:20:09 +0530

    Allow trustedIDs to grade records

--- scripts/logsheet.js
+++ scripts/logsheet.js
@@ -126,8 +126,8 @@
     logSheet.gradeLog = function (sheet_id, vc, grade) {
         return new Promise((resolve, reject) => {
             //reject if user is not subAdmin or editor
-            if (!floGlobals.subAdmins.includes(floDapps.user.id))
-                return reject("Only subAdmins can grade logs")
+            if (!floGlobals.subAdmins.includes(floDapps.user.id) && !floGlobals.trustedIDs.includes(floDapps.user.id))
+                return reject("Only subAdmins/trustedIDs can grade logs")
 
             let log = floGlobals.generalDataset(TYPE_SHEET_ENTRY, {
                 receiverID: sheet_id
